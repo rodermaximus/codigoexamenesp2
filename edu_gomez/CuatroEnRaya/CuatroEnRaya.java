b0db0b853f4cc4c2a36b9271c466df263639754c
edugomez102 <edugomez2299@gmail.com>
2019-06-02 04:43:14 +0200

    no pillo el gitgutter

--- edu_gomez/CuatroEnRaya/CuatroEnRaya.java
+++ edu_gomez/CuatroEnRaya/CuatroEnRaya.java
@@ -1,3 +1,31 @@
 public class CuatroEnRaya{
-	
+
+    private Ficha[][] tablero;
+
+    public class CuatroEnRaya(int n1, int n2){
+        int fil = 8;
+        int col = 6;
+       
+        if(n1 > fil){
+            fil = n1;
+        } 
+        if(n2 > col){
+            col = n2;
+        }
+        tablero = new Ficha[fil][col];
+    }
+    public void iniciaTablero(){
+        for (int i=0;i<tablero.length;i++ ){
+            for(int j=0 ; j<tablero[i].length;j++){
+
+
+            }        
+        } 
+    }
+
+    public void ponFicha(){
+
+    }
+
+
 }
