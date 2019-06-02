addf811b693320004d1834e9005fef60f63fd275
edugomez102 <edugomez2299@gmail.com>
2019-06-02 10:43:27 +0200

    bastante a tope

--- edu_gomez/CuatroEnRaya/CuatroEnRaya.java
+++ edu_gomez/CuatroEnRaya/CuatroEnRaya.java
@@ -13,19 +13,28 @@ public class CuatroEnRaya{
             col = n2;
         }
         tablero = new Ficha[fil][col];
+        tablero = new Ficha[fil][col];
     }
     public void iniciaTablero(){
         for (int i=0;i<tablero.length;i++ ){
-            for(int j=0 ; j<tablero[i].length;j++){
-
-
-            }        
-        } 
+            for(int j=0 ; j<tablero[i].length;j++){  
+                tablero[i][j] = null;
+        }
     }
 
-    public void ponFicha(){
+    public void ponFicha(Ficha f, int col){
+        boolean res = false;
+        int cont = 0;
+        if(f != null && f.getColor != null && col >= tablero.length && col <= tablero[]){
+            for(int i=0;i<tablero.length[0];i++){
+                for (int j=0;i<tablero.length[i] ; ) {
+                    if(tablero[i] != null && i=col){
 
+                    }
+                }
+                
+            }
+
+        }
     }
 
-
-}
---
+++ edu_gomez/Diferencias/Diferencias.java
@@ -0,0 +1,14 @@
+import java.io.*
+
+public class Diferencias{
+	public static void main(String[] args) {
+		// los reader y writer
+		
+		try{
+			
+		}
+		catch (Exception e) {
+		 	
+		} 
+	}
+}
